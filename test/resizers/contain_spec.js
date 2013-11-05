87de18c10f1f2012816d38eea69cc006ff8e3e0c
Paolo Chiodi <chiodi84@gmail.com>
2013-11-05 07:17:49 +0100

    added specs for contain and cover

---
+++ test/resizers/contain_spec.js
@@ -0,0 +1,26 @@
+var Contain = require('../../lib/resizers/contain');
+var Transform = require('stream').Transform;
+
+describe("Contain", function() {
+
+  it("should be a Transform stream", function() {
+    var resizer = new Contain({ height: 100, width: 200 });
+    expect(resizer).to.be.an.instanceof(Transform);
+  });
+
+  it("with both height and width should generate the parameters correctly", function() {
+    var resizer = new Contain({ height: 100, width: 200 });
+    expect(resizer.getParameters()).to.deep.equal(['-', '-size', '200x100>', '-thumbnail', '200x100>', '-']);
+  });
+
+  it("with only height should generate the parameters correctly", function() {
+    var resizer = new Contain({ height: 100 });
+    expect(resizer.getParameters()).to.deep.equal(['-', '-size', 'x100>', '-thumbnail', 'x100>', '-']);
+  });
+
+  it("with only width should generate the parameters correctly", function() {
+    var resizer = new Contain({ width: 200 });
+    expect(resizer.getParameters()).to.deep.equal(['-', '-size', '200x>', '-thumbnail', '200x>', '-']);
+  });
+
+});
---
+++ test/resizers/cover_spec.js
@@ -0,0 +1,16 @@
+var Cover = require('../../lib/resizers/cover');
+var Transform = require('stream').Transform;
+
+describe("Cover", function() {
+
+  it("should be a Transform stream", function() {
+    var resizer = new Cover({ height: 100, width: 200 });
+    expect(resizer).to.be.an.instanceof(Transform);
+  });
+
+  it("with both height and width should generate the parameters correctly", function() {
+    var resizer = new Cover({ height: 100, width: 200 });
+    expect(resizer.getParameters()).to.deep.equal(['-', '-thumbnail', '200x100>^', '-gravity', 'center', '-extent', '200x100>', '-']);
+  });
+
+});
