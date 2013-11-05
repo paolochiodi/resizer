9caab281391f038b7e13759f86997fa29fe67691
Paolo Chiodi <chiodi84@gmail.com>
2013-11-05 07:30:24 +0100

    generate high quality images, auto orient and remove unnecessary profiles

--- test/resizers/contain_spec.js
+++ test/resizers/contain_spec.js
@@ -10,17 +10,17 @@ describe("Contain", function() {
 
   it("with both height and width should generate the parameters correctly", function() {
     var resizer = new Contain({ height: 100, width: 200 });
-    expect(resizer.getParameters()).to.deep.equal(['-', '-size', '200x100>', '-thumbnail', '200x100>', '-']);
+    expect(resizer.getParameters()).to.deep.equal(['-', '-size', '200x100>', '-thumbnail', '200x100>', '-quality', '91', '+profile', '*', '-auto-orient', '-']);
   });
 
   it("with only height should generate the parameters correctly", function() {
     var resizer = new Contain({ height: 100 });
-    expect(resizer.getParameters()).to.deep.equal(['-', '-size', 'x100>', '-thumbnail', 'x100>', '-']);
+    expect(resizer.getParameters()).to.deep.equal(['-', '-size', 'x100>', '-thumbnail', 'x100>', '-quality', '91', '+profile', '*', '-auto-orient', '-']);
   });
 
   it("with only width should generate the parameters correctly", function() {
     var resizer = new Contain({ width: 200 });
-    expect(resizer.getParameters()).to.deep.equal(['-', '-size', '200x>', '-thumbnail', '200x>', '-']);
+    expect(resizer.getParameters()).to.deep.equal(['-', '-size', '200x>', '-thumbnail', '200x>', '-quality', '91', '+profile', '*', '-auto-orient', '-']);
   });
 
 });
