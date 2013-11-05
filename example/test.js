8eb203f94c550f399d22c0f5357f0ee7aa762fc7
Paolo Chiodi <chiodi84@gmail.com>
2013-11-05 07:49:11 +0100

    added example for crop function

--- example/test.js
+++ example/test.js
@@ -2,12 +2,15 @@ var fs = require('fs');
 var resizers = require('../index');
 var Cover = resizers.cover;
 var Contain = resizers.contain;
+var Crop = resizers.crop;
 
 var inputImage = fs.createReadStream(__dirname + '/test.jpg');
 var coverImage = fs.createWriteStream(__dirname + '/cover.jpg');
 var containImage = fs.createWriteStream(__dirname + '/contain.jpg');
 var containImageSingle = fs.createWriteStream(__dirname + '/contain_single.jpg');
+var cropImage = fs.createWriteStream(__dirname + '/crop.jpg');
 
 inputImage.pipe(Cover({height: 100, width: 200})).pipe(coverImage);
 inputImage.pipe(Contain({height: 100, width: 200})).pipe(containImage);
 inputImage.pipe(Contain({width: 200})).pipe(containImageSingle);
+inputImage.pipe(Crop({height: 300, width: 300})).pipe(cropImage);
