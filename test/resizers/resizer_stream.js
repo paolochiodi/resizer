869ab62caaba1dafb0145ae82e237b8334b184dc
Paolo Chiodi <chiodi84@gmail.com>
2013-11-08 19:20:40 +0100

    messages in error events

--- test/resizers/resizer_stream.js
+++ test/resizers/resizer_stream.js
@@ -1,18 +1,19 @@
 var Cover = require('../../lib/resizers/resizer_stream');
 var Stream = require('stream');
+var process = require('child_process');
 
 
-function fakeConvertAndEmit(eventName) {
+function fakeConvertAndEmit(eventName, eventMessage) {
   var EventEmitter = require('events').EventEmitter;
   var convert = new EventEmitter();
 
   convert.stdin = new Stream.Transform();
   convert.stdout = convert.stdin;
-  convert.stderr = new Stream.Readable();
+  convert.stderr = new Stream.PassThrough();
 
   convert.stdin._transform = function(chunk, encoding, cb) {
     this.push(chunk);
-    convert.emit(eventName);
+    convert.emit(eventName, eventMessage);
     cb();
   };
 
@@ -21,21 +22,27 @@ function fakeConvertAndEmit(eventName) {
 
 describe("ResizerStream", function() {
 
+  afterEach(function() {
+    process.spawn.restore && process.spawn.restore();
+  });
+
   it("should be a Transform stream", function() {
     var resizer = new Cover({ height: 100, width: 200 });
     expect(resizer).to.be.an.instanceof(Stream.Transform);
   });
 
   it("should emit an error if graphics magick exits with code != 0", function(end) {
-    var process = require('child_process');
     var resizer = new Cover({ height: 100, width: 200 });
 
     sinon.stub(process, 'spawn', function() {
-      return fakeConvertAndEmit('exit');
+      var convert = fakeConvertAndEmit('exit', 1);
+      convert.stderr.write('error message');
+
+      return convert;
     });
 
-    resizer.on('error', function() {
-      process.spawn.restore();
+    resizer.on('error', function(message) {
+      expect(message).to.be.equal('GM process exited with code 1\nstderr:\nerror message');
       end();
     });
 
@@ -47,11 +54,11 @@ describe("ResizerStream", function() {
     var resizer = new Cover({ height: 100, width: 200 });
 
     sinon.stub(process, 'spawn', function() {
-      return fakeConvertAndEmit('error');
+      return fakeConvertAndEmit('error', 'error message');
     });
 
-    resizer.on('error', function() {
-      process.spawn.restore();
+    resizer.on('error', function(message) {
+      expect(message).to.be.equal('error message');
       end();
     });
 
