1e2d64acce327090bc0895db25b10a9901253f21
Paolo Chiodi <chiodi84@gmail.com>
2013-11-12 09:32:34 +0100

    added benchmarks

--- Makefile
+++ Makefile
@@ -10,6 +10,9 @@ leak:
 ci:
 	./node_modules/.bin/mocha --recursive --watch test
 
+benchme:
+	node benchmark/benchme.js
+
 jshint:
 	find lib -name "*.js" -print0 | xargs -0 ./node_modules/.bin/jshint
 	find test -name "*.js" -print0 | xargs -0 ./node_modules/.bin/jshint
---
+++ benchmark/benchme.js
@@ -0,0 +1,146 @@
+var fs = require('fs');
+var Resizer = require('../index.js');
+var Suite = require('benchmark').Suite;
+var suite = new Suite();
+
+function buildFunction(resizerName, options, image, result) {
+  if(!result) {
+    result = 'output.jpg';
+  }
+
+  return (function(deferred) {
+    var input = fs.createReadStream(__dirname + '/fixtures/' + image);
+    var output = fs.createWriteStream(__dirname + '/fixtures/' + result);
+
+    var resizer = new Resizer[resizerName](options);
+
+    resizer.on('exit', function() {
+      deferred.resolve();
+    });
+
+    resizer.on('error', function(message) {
+      console.log('error', message);
+    });
+
+    input.pipe(resizer).pipe(output);
+  });
+}
+
+function buildContain(image, result) {
+  return buildFunction('contain', { height: 100, width: 100 }, image, result);
+}
+
+function buildContainSingle(image, result) {
+  return buildFunction('contain', { width: 100 }, image, result);
+}
+
+function buildCover(image, result) {
+  return buildFunction('cover', { height: 100, width: 100 }, image, result);
+}
+
+function buildCrop(image, result) {
+  return buildFunction('crop', { height: 100, width: 100 }, image, result);
+}
+
+suite.add({
+  name: 'Contain',
+  defer: true,
+  fn: buildContain('standard.jpg')
+});
+
+suite.add({
+  name: 'Contain Big Image',
+  defer: true,
+  fn: buildContain('big.jpg')
+});
+
+suite.add({
+  name: 'Contain High DPI',
+  defer: true,
+  fn: buildContain('high_dpi.jpg')
+});
+
+suite.add({
+  name: 'Contain PNG',
+  defer: true,
+  fn: buildContain('big.png', 'output.png')
+});
+
+suite.add({
+  name: 'Contain Single',
+  defer: true,
+  fn: buildContainSingle('standard.jpg')
+});
+
+suite.add({
+  name: 'Contain Single Big Image',
+  defer: true,
+  fn: buildContainSingle('big.jpg')
+});
+
+suite.add({
+  name: 'Contain Single High DPI',
+  defer: true,
+  fn: buildContainSingle('high_dpi.jpg')
+});
+
+suite.add({
+  name: 'Contain Single PNG',
+  defer: true,
+  fn: buildContainSingle('big.png', 'output.png')
+});
+
+suite.add({
+  name: 'Cover',
+  defer: true,
+  fn: buildCover('standard.jpg')
+});
+
+suite.add({
+  name: 'Cover Big Image',
+  defer: true,
+  fn: buildCover('big.jpg')
+});
+
+suite.add({
+  name: 'Cover High DPI',
+  defer: true,
+  fn: buildCover('high_dpi.jpg')
+});
+
+suite.add({
+  name: 'Cover PNG',
+  defer: true,
+  fn: buildCover('big.png', 'output.png')
+});
+
+suite.add({
+  name: 'Crop',
+  defer: true,
+  fn: buildCrop('standard.jpg')
+});
+
+suite.add({
+  name: 'Crop Big Image',
+  defer: true,
+  fn: buildCrop('big.jpg')
+});
+
+suite.add({
+  name: 'Crop High DPI',
+  defer: true,
+  fn: buildCrop('high_dpi.jpg')
+});
+
+suite.add({
+  name: 'Crop PNG',
+  defer: true,
+  fn: buildCrop('big.png', 'output.png')
+});
+
+
+suite.on('cycle', function(event) {
+  console.log(String(event.target));
+});
+
+suite.run({async: true});
